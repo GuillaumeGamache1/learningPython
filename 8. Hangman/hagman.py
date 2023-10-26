541aa643b67acc6eabda786e43655f531747fc87
Guillaume <gamacheguillaume96@gmail.com>
2023-10-26 00:31:55 -0400

    added functionalities to the hangman project like updating the word and not allowing the user to guess the same letter multiple times

--- 8. Hangman/hagman.py
+++ 8. Hangman/hagman.py
@@ -5,6 +5,9 @@ words = {
     'sevenLetterWords': ['amazing', 'journey', 'whistle', 'fantasy', 'rainbow'] 
 }
 
+guessedLetters = []
+displayWord= ''
+
 def pickWord():
  x = random.choice(list(words.keys()))
  word_list = words[x]
@@ -22,34 +25,59 @@ def loseLife(answer, life):
 
 def askLetter():
     while True:
-        letter = input("Enter a letter of the alphabet as a guess: " + '\n')
+        letter = input('\n'+"Enter a letter of the alphabet as a guess: " + '\n')
         if len(letter) == 1 and letter.isalpha():
-            return letter
+            if letter in guessedLetters:
+                print("You've already guessed that letter! ")
+            else:
+                guessedLetters.append(letter) 
+                return letter
         else:
             print("Please enter a single letter of the alphabet!" + '\n')
 
 def checkGuess(letter, word):
     if letter.upper() not in word.upper():
-        print("Wrong")
+        print('\n'+"Wrong")
         return False
         
     else:
-        print("Yes it's in the word!")
+        print('\n'+"Yes it's in the word!")
         return True
 
-life = 5
+def updateDisplayWord(word, guessedLetters):
+    display = ''
+    for letter in word:
+        if letter.lower() in guessedLetters:
+            display += letter
+        else:
+            display += '_'
+    return display
+
+
+life = 8
 
 def main():
 
     global life
+    global guessedLetters
+    global displayWord
    
-    print("Welcome to hangman game" + "\n" + "you need to guess the a word" + "\n" + "Give me letters and I'll tell you if it's part of the word"
-    + "\n" + " you have 5 guesses.")
+    print('\n' + "Welcome to hangman game" + "\n" + "You need to guess the word" + "\n" + "Give me letters and I'll tell you if it's part of the word"
+    + "\n" + "you have 8 lives (guesses) and you lose 1 for each wrong guess.")
     hangmanWord = pickWord()
     while life >0: 
+        print("Current word: " + displayWord)
+        if displayWord.lower() ==  hangmanWord.lower():
+            print('\n'+ "Congratulations, you've guessed the word!")
+            break
         userGuess = askLetter()
         answer = checkGuess(userGuess, hangmanWord)
         life = loseLife(answer, life)
+        displayWord = updateDisplayWord(hangmanWord, guessedLetters)
+    
+    if life == 0:
+        print('\n'+ "Dang it! You lost!")
+        print('\n'+ "The word was: " + hangmanWord)
    
 
 main()
